fedd8c3e71932666b6e459dcac4d6fcbbfda26d2
sosaagbontaen <sosaagbontaen@college.harvard.edu>
2022-06-27 15:18:02 -0700

    Implemented Login & Signup

---
+++ Instagram/LoginViewController.m
@@ -0,0 +1,109 @@
+//
+//  ViewController.m
+//  Instagram
+//
+//  Created by Samuel Osa-Agbontaen on 6/27/22.
+//
+
+#import "LoginViewController.h"
+#import "Parse/Parse.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (weak, nonatomic) IBOutlet UIButton *signupButton;
+
+@end
+
+@implementation LoginViewController
+- (IBAction)loginAction:(id)sender {
+    if ([self.usernameField.text isEqual:@""] && [self.passwordField.text isEqual:@""]){
+        [self alert:@"Invalid username and password" messageLabel:@"Username and password fields are empty" leftLabel:@"Cancel" rightLabel:@"OK"];
+        return;
+    }
+    else if ([self.usernameField.text isEqual:@""]){
+        [self alert:@"Invalid username" messageLabel:@"Username field is empty" leftLabel:@"Cancel" rightLabel:@"OK"];
+        return;
+    }
+    else if ([self.passwordField.text isEqual:@""]){
+        [self alert:@"Invalid password" messageLabel:@"Password field is empty" leftLabel:@"Cancel" rightLabel:@"OK"];
+        return;
+    }
+    
+    
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+        
+        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+            if (error != nil) {
+                NSLog(@"User log in failed: %@", error.localizedDescription);
+            } else {
+                NSLog(@"User logged in successfully");
+                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+                
+                // display view controller that needs to shown after successful login
+            }
+        }];
+}
+- (IBAction)signupAction:(id)sender {
+    if ([self.usernameField.text isEqual:@""] && [self.passwordField.text isEqual:@""]){
+        [self alert:@"Invalid username and password" messageLabel:@"Username and password fields are empty" leftLabel:@"Cancel" rightLabel:@"OK"];
+        return;
+    }
+    else if ([self.usernameField.text isEqual:@""]){
+        [self alert:@"Invalid username" messageLabel:@"Username field is empty" leftLabel:@"Cancel" rightLabel:@"OK"];
+        return;
+    }
+    else if ([self.passwordField.text isEqual:@""]){
+        [self alert:@"Invalid password" messageLabel:@"Password field is empty" leftLabel:@"Cancel" rightLabel:@"OK"];
+        return;
+    }
+    
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
+
+- (void)alert:(NSString *)titleLabel messageLabel:(NSString *)messageLabel leftLabel: (NSString *)leftLabel rightLabel: (NSString *)rightLabel{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:titleLabel
+                                                                               message:messageLabel
+                                                                        preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:leftLabel
+                                                        style:UIAlertActionStyleCancel
+                                                      handler:^(UIAlertAction * _Nonnull action) {
+                                                             // handle cancel response here. Doing nothing will dismiss the view.
+                                                      }];
+    [alert addAction:cancelAction];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:rightLabel
+                                                       style:UIAlertActionStyleDefault
+                                                     handler:^(UIAlertAction * _Nonnull action) {
+                                                             // handle response here.
+                                                     }];
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:^{
+        // optional code for what happens after the alert controller has finished presenting
+    }];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
