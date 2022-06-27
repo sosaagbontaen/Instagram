79a5fab56d37be4edb4fb84083b3e6227379b404
sosaagbontaen <sosaagbontaen@college.harvard.edu>
2022-06-27 14:35:38 -0700

    Integrated w/ Parse

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Samuel Osa-Agbontaen on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
